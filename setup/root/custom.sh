9439f75aed802a9132172496c6a669d47da432aa
binhex <megalith01@gmail.com>
2016-11-23 13:11:10 +0000

    cosmetic change to dl package

--- setup/root/custom.sh
+++ setup/root/custom.sh
@@ -4,5 +4,5 @@
 set -e
 
 # manually download stable package from binhex repo (latest deluge on aor is beta/rc)
-curl -o /tmp/deluge-1.3.11-3-any.pkg.tar.xz -L https://github.com/binhex/arch-packages/raw/master/compiled/deluge-1.3.13-1-any.pkg.tar.xz
-pacman -U /tmp/deluge-1.3.11-3-any.pkg.tar.xz --noconfirm
+curl -o /tmp/deluge-1.3.13-1-any.pkg.tar.xz -L https://github.com/binhex/arch-packages/raw/master/compiled/deluge-1.3.13-1-any.pkg.tar.xz
+pacman -U /tmp/deluge-1.3.13-1-any.pkg.tar.xz --noconfirm
